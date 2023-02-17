9429d5d28331f607123f6dd769f6c3437d64fe0a
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-02-17 11:45:03 +0200

    starting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm.jsx/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { GlobalStyle } from '../GlobalStyle';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  addContact = data => {
+    if (
+      this.state.contacts.some(
+        c => c.name.toLowerCase().trim() === data.name.toLowerCase().trim()
+      )
+    ) {
+      alert(`Contact ${data.name} is already exists!`);
+      return;
+    }
+    const newContact = {
+      ...data,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+  handleFilter = ({ target: { value } }) => {
+    this.setState({
+      filter: value,
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().trim().includes(filter.toLowerCase())
+    );
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <div>
+          <h1 style={{ fontSize: '32px' }}>Phone book</h1>
+          <ContactForm addContact={this.addContact} />
+
+          <h2 style={{ fontSize: '32px' }}>Contacts</h2>
+          <Filter onFilterChange={this.handleFilter} value={filter} />
+          <ContactList
+            contacts={filteredContacts}
+            deleteContact={this.deleteContact}
+          />
+        </div>
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
+// чи правильо тут підключені глобальні стилі?
---
+++ src/components/ContactForm.jsx/ContactForm.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { Button } from 'components/Button/Button';
+import { Input } from '../Input/Input';
+export default class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addContact({ ...this.state });
+    this.setState({ name: '', number: '' });
+  };
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <p style={{ fontSize: '32px' }}>Name:</p>
+          <Input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+            value={name}
+          />
+        </label>
+        <label>
+          <p style={{ fontSize: '32px' }}>Number:</p>
+          <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+            value={number}
+          />
+        </label>
+        <Button type="submit">Add contact</Button>
+      </form>
+    );
+  }
+}
+ContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
+//чи треба тут цей проп?
